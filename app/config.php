9dbe597d19eddfff6c89caaf5a9e79dfc1cb76ec
SKYZERKS\Nemo <kwantum03@gmail.com>
2017-04-17 17:37:41 +0300

    first commit

---
+++ app/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$_config = [
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '1111',
+        'database' => 'oopbase'
+    ],
+    'smtp' => [
+        'host' => 'smtp.gmail.com',
+        'port' => 587,
+        'user' => '****@gmail.com',
+        'password' => '****'
+    ],
+    'admin' => [
+        'email' => 'kwantum03@gmail.com'
+    ],
+    'lotLimit'=>"3"
+];
